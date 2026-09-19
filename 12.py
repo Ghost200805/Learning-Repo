f916bdfa3150ce015b39bcc1c9fd53e425739467
Ghost200805 <shamitrajpurohit@gmail.com>
2026-09-19 13:59:55 +0530

    19/09/2026

--- 12.py
+++ 12.py
@@ -5,3 +5,19 @@ for i in range(1,n+1):
     x+=i
 
 print(x)
+
+
+
+
+n = int(input("Enter a number : "))
+# print(len(str(n)))
+
+c=0
+if n == 0:
+    print(1)
+else:
+    while(n>0):
+        c+=1
+        n = n//10
+
+print(c)
