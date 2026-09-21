eaab069d206ba6659caacaf574b49f342a0be7f4
Ghost200805 <shamitrajpurohit@gmail.com>
2026-09-21 10:49:17 +0530

    21/09/2026

--- 13.py
+++ 13.py
@@ -14,4 +14,25 @@ for i in range(n,0,-1):
     for k in range(2 * i - 1):
         print("*", end=" ")
     print()
-    
+
+
+
+
+# New pattern ----
+
+n = 5
+for i in range(1,n+1):     
+    for j in range(n - i):
+        print(" ", end=" ")
+    for k in range(2 * i - 1):
+        print("*", end=" ")
+    print()
+
+
+n = 4
+for i in range(n,0,-1):     
+    for j in range((n - i)+1):
+        print(" ", end=" ")
+    for k in range(2 * i - 1):
+        print("*", end=" ")
+    print()
